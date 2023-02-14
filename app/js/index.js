353af48f2b367bf0b1f203bb76669ffb0fbac08e
basikaka <hellochenpro@hotmail.com>
2023-02-14 22:44:49 +0800

    Lexical this

--- app/js/index.js
+++ app/js/index.js
@@ -5,23 +5,21 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
 
-logTitle('Arrow Functions')
+logTitle('Lexical this')
 
-const hello = () => {
-    const es6 = 'ES6';
-    return `Hello ${es6}`;
-  };
-  
-  const powers = [1,2,3,4,5].map((number, index) =>  Math.pow(number, index));
-  
-  
-  const add = (n1, n2) => n1 + n2;
-  
-  const milesToKm = miles => miles * 1.60934;
-  
-  log(hello());
-  log(powers);
-  log(add(100,100));
-  log(milesToKm(8));
+const person = {
+    name: 'Jone Maria',
+    cars: ['farari', 'lombo'],
+    toString: function () {
+        // og(`${this.name} has ${this.cars}`)
+        const _this = this;
+        this.cars.forEach( car => {
+            log(`${this.name} has ${car}`)
+        })
+    }
+}
+
+
+person.toString()
 
   
